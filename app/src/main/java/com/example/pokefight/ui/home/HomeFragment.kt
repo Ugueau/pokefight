c9486ea1acf7f66bc0041a338a9ffb6ef60f406e
MaelChalon <97522929+MaelChalon@users.noreply.github.com>
2023-11-15 18:31:36 +0100

    fin affichage page principale

--- app/src/main/java/com/example/pokefight/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokefight/ui/home/HomeFragment.kt
@@ -32,7 +32,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(MainViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
