4d523d583bbeaf80dadf5f7d3b837fa502231810
Hugo_Millot <97529289+Ugueau@users.noreply.github.com>
2023-11-22 10:16:39 +0100

    patch limit of fetching

--- app/src/main/java/com/example/pokefight/ui/MainViewModel.kt
+++ app/src/main/java/com/example/pokefight/ui/MainViewModel.kt
@@ -58,27 +58,43 @@ class MainViewModel : ViewModel() {
 
     fun fetchPokemons(fromId: Int = 1, toId: Int = fromId + 10) {
         viewModelScope.launch {
-            val data = PokemonRepository.fetchPokemons(fromId, toId)
-            data.collect {
-                _pokemonLiveData.postValue(it)
+            if (fromId <= PokemonRepository.MAX_ID) {
+                val checkedToId :Int = if(toId > PokemonRepository.MAX_ID){
+                    PokemonRepository.MAX_ID
+                }else{
+                    toId
+                }
+                val data = PokemonRepository.fetchPokemons(fromId, checkedToId)
+                data.collect {
+                    _pokemonLiveData.postValue(it)
+                }
             }
         }
     }
 
     fun getPokemonList(fromId: Int = 1, toId: Int = fromId + 10): LiveData<List<Pokemon>> {
         val liveData = MutableLiveData<List<Pokemon>>()
-        viewModelScope.launch {
-            val data = PokemonRepository.getPokemons(fromId, toId)
-            liveData.postValue(data)
+        if (fromId <= PokemonRepository.MAX_ID) {
+            val checkedToId :Int = if(toId > PokemonRepository.MAX_ID){
+                PokemonRepository.MAX_ID
+            }else{
+                toId
+            }
+            viewModelScope.launch {
+                val data = PokemonRepository.getPokemons(fromId, checkedToId)
+                liveData.postValue(data)
+            }
         }
         return liveData
     }
 
-    fun getPokemonById(id: Int): LiveData<Pokemon> {
-        val liveData = MutableLiveData<Pokemon>()
-        viewModelScope.launch {
-            val data = PokemonRepository.getPokemonById(id)
-            liveData.postValue(data)
+    fun getPokemonById(id: Int): LiveData<Pokemon?> {
+        val liveData = MutableLiveData<Pokemon?>()
+        if (id <= PokemonRepository.MAX_ID) {
+            viewModelScope.launch {
+                val data = PokemonRepository.getPokemonById(id)
+                liveData.postValue(data)
+            }
         }
         return liveData
     }
