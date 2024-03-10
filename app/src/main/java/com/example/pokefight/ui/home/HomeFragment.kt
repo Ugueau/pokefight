e50fc4be4c3fff750829771e4f634947b84f742d
Hugo_Millot <hugomillot2003@gmail.com>
2024-03-10 22:46:26 +0100

    create a qrcode tool class

--- app/src/main/java/com/example/pokefight/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokefight/ui/home/HomeFragment.kt
@@ -1,32 +1,24 @@
 package com.example.pokefight.ui.home
 
-import android.content.Context
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokefight.R
 import com.example.pokefight.databinding.FragmentHomeBinding
+import com.example.pokefight.tools.QRCodeTool
 import com.example.pokefight.ui.MainViewModel
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.qrcode.QRCodeWriter
-import com.journeyapps.barcodescanner.ScanContract
-import com.journeyapps.barcodescanner.ScanIntentResult
-import com.journeyapps.barcodescanner.ScanOptions
 
 class HomeFragment : Fragment() {
 
@@ -38,42 +30,6 @@ class HomeFragment : Fragment() {
     private lateinit var TextViewTrophy : TextView
     private lateinit var FightButton : Button
     private lateinit var QRCodeImage : ImageView
-
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()){
-            isGranted: Boolean ->
-            if(isGranted){
-                showCamera()
-            }
-        }
-
-    private val scanLauncher =
-        registerForActivityResult(ScanContract()){ result: ScanIntentResult ->
-            run {
-                if (result.contents == null){
-                    Toast.makeText(this.context, "Cancelled", Toast.LENGTH_SHORT).show()
-                }else{
-                    setResult(result.contents)
-                }
-            }
-        }
-
-    private fun setResult(contents: String) {
-        Log.i("setResult: ", contents)
-    }
-
-    private fun showCamera() {
-        val option = ScanOptions()
-        option.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
-        option.setPrompt("Scan QR code")
-        option.setCameraId(0)
-        option.setBeepEnabled(false)
-        option.setBarcodeImageEnabled(true)
-        option.setOrientationLocked(false)
-
-        scanLauncher.launch(option)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -95,9 +51,11 @@ class HomeFragment : Fragment() {
         TextViewTrophy = binding.TextViewTrophy
         TextViewTrophy.text = mainViewModel.getConnectedUser().Trophy.toString()
 
+        val qrcodeReader = QRCodeTool(this, requireContext())
+
         FightButton = view.findViewById<Button>(R.id.FightButton)
         FightButton.setOnClickListener {
-            checkCameraPermission(this.context)
+            qrcodeReader.checkCameraPermission()
         }
 
         val connectedUser = vm.getConnectedUser()
@@ -131,19 +89,5 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun checkCameraPermission(context: Context?){
-        if (context != null) {
-            if (ContextCompat.checkSelfPermission(context,android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-                showCamera()
-            }
-            else if(shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)){
-                Toast.makeText(context, "CAMERA permission Riquiered", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
-            }
-        }
     }
 }
